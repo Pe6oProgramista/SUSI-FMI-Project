63f79f21a215f017473a9ddefe7e3bc548430959
Pe6oProgramista <p.milev3@gmail.com>
2020-05-22 04:33:39 +0300

    make smart pointer independent class

---
+++ include/utility/SmrtPtr.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+namespace susi {
+    namespace utils {
+
+        template<class T>
+        class SmrtPtr {
+        private:
+            T* t;
+            unsigned int* count;
+        public:
+            SmrtPtr() : t(nullptr), count(nullptr) {}
+            SmrtPtr(T* t) : t(t) {
+                count = new unsigned int(1);
+            }
+            SmrtPtr(const SmrtPtr& p) : t(p.t), count(p.count) { ++(*count); }
+            ~SmrtPtr() {
+                if(!is_null() && --(*count) == 0) {
+                    delete t;
+                    delete count;
+                }
+            }
+
+            SmrtPtr& operator= (const SmrtPtr& p) {
+                T* const old_t = t;
+                unsigned int* const old_count = count;
+
+                t = p.t;
+                count = p.count;
+                ++(*count);
+
+                if (--(*old_count) == 0) {
+                    delete old_t;
+                    delete old_count;
+                }
+                return *this;
+            }
+            T* operator-> () const { return t; }
+            T& operator* ()  { return *t; }
+            const T& operator* () const { return *t; }
+
+            bool is_null() const { return t == nullptr; }
+        };
+
+    }
+}
